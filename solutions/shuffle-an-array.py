8bb961678a45eba891084ae41a5c72ba98f53d9e
paulinagacek <paulina.gacek.pl@gmail.com>
2025-11-16 09:54:17 +0100

    Faulty solutions removed

--- solutions/shuffle-an-array.py
+++
@@ -1,7 +0,0 @@
-from random import random
-
-
-class Solution(object):
-    def __init__(self, nums):
-        self.reset = lambda: nums
-        self.shuffle = lambda: random.sample(nums, len(nums))
